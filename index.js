6686148b55a5b0614be488835e85f179f027f05d
Maximo Bautista <max.bautista97@gmail.com>
2019-03-22 15:10:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Formatter {
      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
      
     return string.split('').map( word => {
-      exceptions.includes(word) ? return word : return  this.capitalize(word)
+      exceptions.includes(word) ? word : this.capitalize(word)
     })
   }
  
