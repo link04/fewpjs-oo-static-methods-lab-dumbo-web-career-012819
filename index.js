7b4571d97ec26383fe1c20d075a0c1853f61db24
Maximo Bautista <max.bautista97@gmail.com>
2019-03-22 15:23:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string[0].toUpperCase() + string.split('').splice(1).join('');
+  }
+ 
+  static sanitize(string){
+   return string.replace(/[^\w\s\-\']/gi, '')
+  }
+ 
+   static titleize(string){
+     const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+     
+     return string.split(' ').map( word => 
+      exceptions.includes(word) && string[0] != word ? word : this.capitalize(word)
+    ).join(' ')
+  }
+ 
+
 }
